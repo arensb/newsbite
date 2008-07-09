7fab236fc4df0f880d3f7eeda3d0bfea0900b2c8
Andrew Arensburger <arensb@ooblick.com>
2008-07-09 23:56:34 +0000

    Set skin to "default" in config.inc. Set compile_id in Smarty.

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,12 +4,11 @@ require_once("database.inc");
 require_once(SMARTY_DIR . "Smarty.class.php");
 
 $smarty = new Smarty();
-$skin = "default";
 $smarty->template_dir	= "skins/$skin";
+$smarty->compile_id     = "$skin-skin";
 $smarty->compile_dir	= SMARTY_PATH . "templates_c";
 $smarty->cache_dir	= SMARTY_PATH . "cache";
 $smarty->config_dir	= SMARTY_PATH . "configs";
-$smarty->compile_id = "default-skin";
 
 $dbh = db_connect();
 $feeds = db_get_feeds();
