dd1c4e292e607632fe955f73818afa5e8d913715
Andrew Arensburger <arensb@ooblick.com>
2010-05-08 18:10:17 +0000

    Functions for manipulating a DOM element's class list. Use is_in_class() for finding checkbox's item ancestor.

--- htdocs/skins/fancy/view.js
+++ htdocs/skins/fancy/view.js
@@ -500,6 +500,7 @@ function parse_flush_response(req)
 /* mark_item
  * Called when user changes the checkbox on an item, to toggle it from
  * read to unread or vice-versa.
+ * 'elt' is the checkbox that was just checked by the user.
  */
 function mark_item(elt)
 {
@@ -507,7 +508,7 @@ function mark_item(elt)
 	 * chain.
 	 */
 	var item_div = elt.parentNode;
-	while (item_div && (item_div.className != "item"))
+	while (item_div && !is_in_class(item_div, "item"))
 		item_div = item_div.parentNode;
 	if (item_div == null)
 		/* Something's wrong. Abort */
@@ -516,6 +517,7 @@ function mark_item(elt)
 	/* Set the "state" attribute to either "read" or "unread" so
 	 * that the CSS rules can change the appearance appropriately.
 	 */
+	// XXX - Should the "state" attribute be retained?
 	item_div.setAttribute("state", (elt.checked ? "read" : "unread"));
 
 	/* There are two checkboxes for each item. Find the matching
@@ -574,6 +576,189 @@ function mark_item(elt)
 	 */
 	elt.blur();
 }
+
+/* ----------------------------------------
+ * Functions for manipulating a DOM element's class list.
+ *
+ * All of these functions take a DOM element with an attribute of the
+ * form class="foo bar baz", and add/remove/manipulate the class list.
+ */
+
+/* is_in_class
+ * Returns true iff 'cls' is in the class list of 'elem'.
+ */
+function is_in_class(elem, cls)
+{
+	var class_str = elem.getAttribute("class");
+
+	if (class_str == undefined)
+		// No class set
+		return false;
+
+	var classes = class_str.split(" ");
+			// Split class string into a list
+
+	for (var i in classes)
+	{
+		if (classes[i] == cls)
+			// Yup. It's in 'cls'.
+			return true;
+	}
+	return false;	// Nope. It's not in 'cls'
+}
+
+/* add_class
+ * Make sure 'cls' is on the class list of 'elem', adding it if
+ * necessary.
+ */
+function add_class(elem, cls)
+{
+	var class_str = elem.getAttribute("class");
+
+	if (class_str == undefined)
+		// No class set
+		return false;
+
+	var classes = class_str.split(" ");
+			// Split class string into a list
+
+	for (var i in classes)
+	{
+		if (classes[i] == cls)
+			// Element already has this class
+			return;
+	}
+
+	// Need to add the class
+	elem.setAttribute("class",
+			  classes.concat([cls]).join(" "));
+}
+
+/* remove_class
+ * Remove 'cls' from the class list of 'elem'.
+ */
+function remove_class(elem, cls)
+{
+	var class_str = elem.getAttribute("class");
+
+	if (class_str == undefined)
+		// No class set
+		return false;
+
+	var classes = class_str.split(" ");
+			// Split class string into a list
+	var new_classes = new Array();
+			// The new class list
+
+	for (var i in classes)
+	{
+		if (classes[i] == cls)
+			// Skip over the class we're removing
+			continue;
+		new_classes.push(classes[i]);
+			// Remember this other class
+	}
+
+	// The new class list is whatever we're left with.
+	elem.setAttribute("class", new_classes.join(" "));
+}
+
+/* replace_class
+ * Replaces 'old_class' with 'new_class' in the class list of 'elem'.
+ * This is logically equivalent to
+ *	remove_class(elem, old_class);
+ *	add_class(elem, new_class);
+ * but is more efficient, since it combines the two.
+ */
+function replace_class(elem, old_class, new_class)
+{
+	var class_str = elem.getAttribute("class");
+
+	if (class_str == undefined)
+		// No class set
+		return false;
+
+	var classes = class_str.split(" ");
+			// Split class string into a list
+	var seen_new_class = false;
+			// Have we seen the new class already?
+
+	for (var i in classes)
+	{
+		if (classes[i] == old_class)
+			// Don't add old_class to new_classes
+			continue;
+		if (classes[i] == new_class)
+			// Note that we've seen new_class on the way
+			seen_new_class = true;
+	}
+
+	// If new_class is already on the class list, don't add it a
+	// second time.
+	if (!seen_new_class)
+		classes.push(new_class);
+
+	// The new class list is whatever we're left with.
+	elem.setAttribute("class", classes.join(" "));
+}
+
+/* toggle_class
+ * If 'elem' has 'classA', replace that with 'classB', and vice-versa.
+ * This is logically equivalent
+ */
+function toggle_class(elem, classA, classB)
+{
+	var class_str = elem.getAttribute("class");
+
+	if (class_str == undefined)
+		// No class set
+		return false;
+
+	var classes = class_str.split(" ");
+			// Split class string into a list
+	var new_classes = new Array();
+	var toggled = false;
+			
+
+	for (var i in classes)
+	{
+		if (classes[i] == classA)
+		{
+			// We've found classA
+			if (toggled)
+				// We've already seen classA or classB.
+				// Don't add this one a second time.
+				continue;
+
+			// We're toggling from classA to classB, so
+			// put classB on the new list of classes.
+			new_classes.push(classB);
+			toggled = true;
+				// Remember that we've done this
+			continue;
+		}
+		if (classes[i] == classB)
+		{
+			// We've found classB
+			if (toggled)
+				// We've already seen classA or classB.
+				// Don't add this one a second time.
+				continue;
+
+			// We're toggling from classB to classA, so
+			// put classA on the new list of classes.
+			new_classes.push(classA);
+			toggled = true;
+				// Remember that we've done this
+			continue;
+		}
+		new_classes.push(classes[i]);
+	}
+
+	// The new class list is whatever we're left with.
+	elem.setAttribute("class", new_classes.join(" "));
+}
+/* ---------------------------------------- */
 
 // Mark all global functions for profiling.
 //for (f in window)
