c8c7379cee848d4525eaa5dfa8f1161386424ebd
Andrew Arensburger <arensb@ooblick.com>
2010-05-29 16:30:29 +0000

    Moved Dublin Core stuff out of lib/parse-feed.inc and into plugins/xml-dc.php. Cleaned up list of namespaces in parse-feed.inc.

---
+++ plugins/xml-dc.php
@@ -0,0 +1,45 @@
+<?
+/* Plugin for the RSS Dublin Core extension:
+ * Handle the RSS Dublin Core extension:
+ * http://purl.org/dc/elements/1.1/
+ * http://dublincore.org/documents/dces/
+ *
+ * <dc:creator>name</dc:creator>
+ *	The author of the post
+ */
+
+/* dc_element_handler
+ */
+function dc_element_handler(
+	$ns_url,	// URL of this element's namespace
+	$prefix,	// Namespace of this element
+	$elt_name,	// Name of this element, without prefix
+	&$attrs,	// Attributes
+	&$children,	// Contents of this element
+	&$retval,	// Fill in the blanks here
+	&$context)	// Where we are at the moment
+{
+	$parent = $context[count($context)-1]['name'];
+	switch ($elt_name)
+	{
+	    case "creator":
+		// XXX - Make sure $chldren is a string?
+		$retval['author_name'] = $children;
+		break;
+
+	    default:
+//		echo "Warning: unknown \"dc:\" element: [$elt_name]\n";
+		break;
+		
+	}
+
+	return true;
+}
+
+/* Register this handler with the XML parser */
+add_xml_handler("http://purl.org/dc/elements/1.1/",
+		array(
+			"element"	=> dc_element_handler,
+		)
+	);
+?>
