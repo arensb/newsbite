40d39613879eec01c66c9dbe449253bce5b31457
Andrew Arensburger <arensb@ooblick.com>
2010-09-20 13:40:53 +0000

    Load hooks, so skins can take advantage of them.

--- htdocs/editfeed.php
+++ htdocs/editfeed.php
@@ -5,6 +5,9 @@
 require_once("common.inc");
 require_once("database.inc");
 require_once("skin.inc");
+require_once("hooks.inc");
+
+load_hooks(PLUGIN_DIR);
 
 $feed_id = $_REQUEST['id'];		// ID of feed to show
 /* Make sure $feed_id is an integer */
