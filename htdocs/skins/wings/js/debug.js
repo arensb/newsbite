f74394424737ef6a2b27df4cb3752cf9f06f6b05
Andrew Arensburger <arensb@ooblick.com>
2010-09-21 14:15:42 +0000

    Copied from fancy skin.

---
+++ htdocs/skins/wings/js/debug.js
@@ -0,0 +1,33 @@
+/* debug.js
+ * Debugging functions.
+ */
+#ifndef _debug_js_
+#define _debug_js_
+
+var debug_window = undefined;
+
+var last_time = null;
+function debug(str)
+{
+/*return;*/
+	if (debug_window == undefined)
+		debug_window = document.getElementById("debug");
+	if (debug_window == null)
+		return;
+
+	var t = new Date().getTime();
+//	var delta = (last_time == null ? "" : t - last_time);
+//	last_time = t;
+//	debug_window.innerHTML += t + "(" + delta + "): " +
+	debug_window.innerHTML += t + ": " +
+		str + "<br/>\n";
+}
+
+function clrdebug()
+{
+	if (debug_window == null)
+		return;
+	debug_window.innerHTML = "";
+}
+
+#endif	// _debug_js_
