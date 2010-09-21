bdc41760d3e852bc353957a8b8bfbd4a4b33a653
Andrew Arensburger <arensb@ooblick.com>
2010-09-21 14:15:42 +0000

    Copied from fancy skin.

---
+++ htdocs/skins/wings/js/xhr.js
@@ -0,0 +1,41 @@
+/* xhr.js
+ * A browser-independent way of creating XMLHttpRequest objects.
+ */
+#ifndef _xhr_js_
+#define _xhr_js_
+
+/* createXMLHttpRequest
+ * Create a new XMLHttpRequest object, hopefully in a
+ * browser-independent manner.
+ */
+function createXMLHttpRequest()
+{
+	var request = false;
+
+	/* Firefox, Safari, etc. */
+	if (window.XMLHttpRequest)
+	{
+		if (typeof XMLHttpRequest != 'undefined')
+		{
+			try {
+				request = new XMLHttpRequest();
+			} catch (e) {
+				request = false;
+//				debug("Error allocating new XMLHttpRequest\n");
+			}
+		}
+	} else if (window.ActiveXObject)
+	{
+		/* IE */
+		/* Create a new ActiveX XMLHTTP object */
+		try {
+			request = new ActiveXObject('Msxml2.XMLHTTP');
+		} catch (e) {
+			request = false;
+//			debug("Error allocating ActiveX XMLHTTP\n");
+		}
+	}
+	return request;
+}
+
+#endif	// _xhr_js_
