bf780622fff91ae87eae7ec6678df051cd1065e3
Andrew Arensburger <arensb@ooblick.com>
2010-10-02 16:51:06 +0000

    Anal-er HTML header. Added tools page. Added link to tools page. Added debug div.

--- htdocs/skins/wings/feeds.php
+++ htdocs/skins/wings/feeds.php
@@ -6,7 +6,7 @@ $skin_dir = $skin_vars['skin'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <title>NewsBite</title>
 <link rel="stylesheet" type="text/css" href="skins/<?=$skin_dir?>/style.css" media="all" />
@@ -41,10 +41,18 @@ switch ($skin_vars['mobile'])
 <!-- Index page shows list of feeds -->
 <h1>Feeds</h1>
 
+<div>
+<ul>
+<li><a href="javascript:flip_to_page('tool-page')">Tools</a></li>
+</ul>
+</div>
+
 <!-- XXX - The feeds div should start out with a "please wait" spinner
      until the data is loaded.
  -->
 <div id="feeds"></div>
+
+<div id="debug"></div>
 
 <noscript>
 <p>Sorry, but you really, really need JavaScript turned on to get anywhere
@@ -67,5 +75,16 @@ with this skin.</p>
 <a href="javascript:flip_to_page('index-page')">Index</a>
 </div><!-- End of article page -->
 
+<div class="page" id="tool-page">
+<!-- Tool page shows various admin tools -->
+<h1>Admin Tools</h1>
+
+<a href="javascript:flip_to_page('index-page')">Index</a>
+
+<ul>
+<li><a href="javascript:localStorage.clear()">Clear local storage</a></li>
+</ul>
+</div><!-- End of tool page -->
+</div><!-- End of page-list" -->
 </body>
 </html>
