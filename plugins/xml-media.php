d9032f65a64eb6013db8ed5a128457ac60320b15
Andrew Arensburger <arensb@ooblick.com>
2010-10-22 13:25:26 +0000

    "To do" comments

--- plugins/xml-media.php
+++ plugins/xml-media.php
@@ -20,12 +20,29 @@ function media_element_handler(
 		# Thumbnail for a post, presumably
 		$retval['media:thumbnail'] = $attrs['url'];
 		break;
-		// XXX - group
-		// XXX - content
+		// XXX - Other elements seen in the wild:
+		// category - Ignore
+		// content - Attached pictures/audio/whatever
+		// credit - Include in caption?
+		// description - Hover text? Caption?
+		// group - Group of related images
+		// keywords - Ignore
+		// player - Useful for video
+		// thumbnail - Perhaps use for non-image media
+		// title - Hover text? Caption?
+		//
+		// XXX - Watch out with media:player: the example at
+		// http://video.search.yahoo.com/mrss uses it as a
+		// singleton, but the only place I've seen it in the
+		// wild is at blip.tv, which puts a CDATA section
+		// inside (giving the <embed> element to use.
+	    case "content":
+		// XXX
+		break;
 	    default:
-		    // Ignore
-		    // XXX - Is there anything else we'd want?
-		    break;
+		// Ignore
+		// XXX - Is there anything else we'd want?
+		break;
 	}
 
 	return true;
