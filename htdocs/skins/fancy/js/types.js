bfc668c3af890c53d5e69add387b3a475813b32f
Andrew Arensburger <arensb@ooblick.com>
2011-10-29 20:29:40 +0000

    displaytitle: memoize return value (using _displaytitle). Better check for empty title. sortname: memoize return value (using _sortname). Added some real content (for English).

--- htdocs/skins/fancy/js/types.js
+++ htdocs/skins/fancy/js/types.js
@@ -19,6 +19,7 @@ function Feed(arg)
 }
 
 Feed.prototype = {
+	// From database:
 	// id (int)
 	// title
 	// subtitle
@@ -30,6 +31,10 @@ Feed.prototype = {
 	// ttl
 	// image (url to image)
 	// active (bool)
+
+	// Other fields:
+	// _displaytitle (cached value from displaytitle())
+	// _sortname (cached value from sortname())
 };
 
 /* displaytitle
@@ -39,12 +44,22 @@ Feed.prototype = {
  */
 Feed.prototype.displaytitle = function()
 {
+	if (this._displaytitle != undefined)
+		// Memoized value
+		return this._displaytitle;
+
 	if (this.nickname != null && this.nickname != "")
-		return this.nickname;
-	else if (this.title != null && this.title != "")
-		return this.title;
+		// Use the user-specified nickname
+		this._displaytitle = this.nickname;
+	else if (this.title != null && !this.title.match(/^\s*$/))
+		// Use the non-empty name that the feed has chosen for
+		// itself
+		this._displaytitle = this.title;
 	else
-		return "[no title]";
+		// Use a placeholder name
+		this._displaytitle = "[no title]";
+
+	return this._displaytitle;
 }
 
 /* sortname
@@ -52,16 +67,53 @@ Feed.prototype.displaytitle = function()
  */
 Feed.prototype.sortname = function()
 {
-return;
-//	if (this.sortname != undefined)
-//		// Memoized value
-//		return this.sortname;
-//
-//	var sortname = this.displaytitle();
-//	// XXX - See mkalbum.
+	if (this._sortname != undefined)
+		// Memoized value
+		return this._sortname;
+
+	/* XXX - This sorting is very English-centric. Ought to allow
+	 * different rules for different languages.
+	 */
+	var sorted = this.displaytitle();
+	sorted = sorted.toLocaleUpperCase();
+			// Pretty much the same as toUpperCase(),
+			// except in Turkish, but hey, i18n is good.
+	sorted = sorted.replace(/^(THE|A|AN|OF)\b\s*(.*)$/, "$2 $1");
+			// Move "The" and others to the end
+	sorted = sorted.replace(/\'(S|RE|LL)\b/g, "$1");
+			// Remove apostrophe in "John's", "we're", "they'll"
+	sorted = sorted.replace(/N\'T\b/g, "NT");
+			// Remove apostrophe in "isn't", "hasn't", etc.
+	sorted = sorted.replace(/&/g, " AND ");
+	sorted = sorted.replace(/-/g, " ");
+			// Hyphen becomes space.
+	sorted = sorted.replace(/[^\w\s]/g, "");
+			// Remove weird characters
+	sorted = sorted.replace(/\s+/g, " ");
+			// Collapse multiple spaces into one
+	sorted = sorted.replace(/^\s+/, "");
+			// Remove leading whitespace
+	sorted = sorted.replace(/\s+$/, "");
+			// Remove trailing whitespace
+	this._sortname = sorted;
+			// Memoize value for next time
+	return sorted;
+			// XXX - Could also chain calls:
+			// return this.displaytitle()
+			//	.toLocaleUpperCase()
+			//	.replace(...)
+			//	...
+			// From the discussions I've seen, this is
+			// less a matter of efficiency than of style
+			// and readability. Do chained calls make the
+			// code more readable?
 }
 
 // XXX - Would it be useful to have a FeedList class, for an array of Feeds?
+// - Look up an existing feed by name, ID, whatever
+// - Update an existing feed from a { } object.
+// - Are details/tools/empty feeds/stale feeds currently displayed?
+// - How are we sorting?
 
 /* XXX - Item class
  * Class based on 'items' table in the database.
