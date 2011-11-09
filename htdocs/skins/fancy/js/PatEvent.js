5f545ba80c5ba7325a530ba9ddadfb8a7c358870
Andrew Arensburger <arensb@ooblick.com>
2011-11-09 05:01:51 +0000

    Major changes. Almost a complete rewrite.

--- htdocs/skins/fancy/js/PatEvent.js
+++ htdocs/skins/fancy/js/PatEvent.js
@@ -1,36 +1,40 @@
-#ifndef _Events_js_
-#define _Events_js_
-/* Events.js
- * Interface for binding events to handlers, with selectors.
+#ifndef _PatEvent_js_
+#define _PatEvent_js_
+/* XXX - Currently doesn't support changing an existing DOM node. This
+ * matters because we might have
+ *	<div class="active">
+ *	  <div class="thing">
+ *	</div>
+ * and we might want to attach an event to elements that match
+ * ".active .thing". But if at some point we change 'class="active"'
+ * to 'class="inactive"', then the inner <div> will no longer match
+ * ".active .thing", and the Right Thing to do would be to remove the
+ * event. But that's a hard problem, especially since there's no way
+ * to list the EventListeners attached to a given node.
+ *
+ * Perhaps could attach to each node a list of all event handlers.
  */
-/* XXX - Bleah. This doesn't work. Redo.
- */
-/* XXX - Potential problem (or security hole, or something):
- * If we have bind_event("click", ".foo", some_handler)
- * and a post includes <div class="foo">...</div>
- * then some_handler() will be invoked when the user clicks on that
- * div. Ought to have some way of setting up forbidden areas that will
- * not have handlers attached to them. Perhaps have
- *	dont_bind(".item .summary");
- *	dont_bind(".item .content");
- * to say not to attach any listeners to matching DOM nodes (or their
- * children).
- */
-document.addEventListener("DOMContentLoaded", init_Events, false);
+var PatEvent;
 
-var event_bindings = {};	// Table of event handlers, a
+// Don't include this module twice, 'cos you'll get clobbered.
+PatEvent = {
+	bindings:	{},	// Table of event handlers, a
 				// 2-dimensional hash:
-				// event_bindings[evtype][selector] => handler
+				// bindings[selector][evtype][] => handler
+	};
+// We also add a DOMContentLoaded listener at the end, to
+// initialize stuff.
+// XXX - Is there a better way to initialize it?
 
-/* init_Events
- * Initialize the Event-related stuff.
+/* _init
+ * Initialize the PatEvent-related stuff.
  */
-/* XXX - Should this be made a constructor of some kind, or invoked
- * automatically?
- */
-function init_Events()
+PatEvent._init = function()
 {
 	/* Find a version of matchesSelector() on this browser */
+	/* XXX - This ought to go elsewhere, in a 'compat' module or
+	 * something.
+	 */
 	if (Element.prototype.matchesSelector != null)
 		/* Everything's okay */
 		;
@@ -46,7 +50,9 @@ function init_Events()
 	// Chrome doesn't support DOMAttrModified. Here's a fix
 	// suggested by Stack Overflow:
 	// http://stackoverflow.com/questions/1882224/is-there-an-alternative-to-domattrmodified-that-will-work-in-webkit
-	if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1)
+	// "AppleWebKit" should catch both Chrome and (Mobile) Safari,
+	// which are the ones I care about.
+	if (navigator.userAgent.toLowerCase().indexOf('applewebkit') > -1)
 	{
 		// Replace the old Element.setAttribute() method with
 		// a wrapper that'll invoke the old method, then
@@ -58,9 +64,6 @@ function init_Events()
 			var e = document.createEvent("MutationEvent"); 
 			var prev = this.getAttribute(name); 
 			this._setAttribute(name, val);
-			/* XXX - What's that 2 (attrChangeArg)?
-			 * Firefox uses 1.
-			 */
 			e.initMutationEvent("DOMAttrModified",
 					    true,
 					    true,
@@ -68,69 +71,203 @@ function init_Events()
 					    prev,
 					    val,
 					    name,
-					    2);
+					    1);	// XXX - See http://help.dottoro.com/ljifcdwx.php
+					        // 1 - Modification
+					        // 2 - Addition
+					        // 3 - Removal
+					        // So need to examine
+					        // old value to see
+					        // what kind of change
+					        // this is.
 			this.dispatchEvent(e);
+		}
+	}
+
+	// Remove initialization listener, for cleanliness
+	document.removeEventListener("DOMContentLoaded", PatEvent._init, false);
+}
+
+/* _enter_handler (see also _exit_handler)
+ * It's useful to be able to say "call this handler when the mouse
+ * crosses over into element X, and call this other handler when the
+ * mouse exits it". The obvious way to handle this is with "mouseover"
+ * and "mouse out", but those are kinda broken in Firefox.
+ *
+ * Let's say you have
+ *	<parent>
+ *	  <child1>...</child1>
+ *	  <child2>...</child1>
+ *	</parent>
+ * and "mouseover" and "mouseout" events attached to <parent>.
+ *
+ * If there's no padding inside <parent>, then "mouseover" will be
+ * triggered on the child, and bubble up to the parent. Furthermore,
+ * when the mouse moves from <child1> to <child2>, that'll trigger a
+ * "mouseout" event to say that the mouse has exited <child1>, and a
+ * "mouseover" event to say that the mouse has entered <child2>. If
+ * the user's handler causes visual effects, this can be annoying and
+ * distracting.
+ *
+ * So _enter_handler() is a wrapper function: it checks to make sure
+ * that the mouse is moving from outside <parent> to inside (and not
+ * from one child to another), and only calls the user's intended
+ * handler when it ought to.
+ */
+PatEvent._enter_handler = function(ev, realhandler)
+{
+	var node = ev.currentTarget;
+
+	// If going from one child to another, ignore this event.
+	var from_elt = ev.relatedTarget;
+	var to_elt = ev.target;
+
+	// If the "to" node isn't one of our children, we can just
+	// ignore this event.
+	while (to_elt != node)
+	{
+		if (to_elt == null)
+			return;
+		to_elt = to_elt.parentNode;
+	}
+ 
+	// If we get this far, the "to" node is one of our children.
+	// If the "from" node is also a child, then ignore this event.
+	while (from_elt != null)
+	{
+		if (from_elt == node)
+			return;
+		from_elt = from_elt.parentNode;
+	}
+
+	realhandler(ev);
+}
+
+PatEvent._exit_handler = function(ev, realhandler)
+{
+	var node = ev.currentTarget;
+
+	// If going from one child to another, ignore this event.
+	var from_elt = ev.target;
+	var to_elt = ev.relatedTarget;
+
+	// If the "from" node isn't one of our children, we can just
+	// ignore this event.
+	while (from_elt != node)
+	{
+		if (from_elt == null)
+			return;
+		from_elt = from_elt.parentNode;
+	}
+
+	// If we get this far, the "from" node is one of our children.
+	// If the "to" node is also a child, then ignore this event.
+	while (to_elt != null)
+	{
+		if (to_elt == node)
+			return;
+		to_elt = to_elt.parentNode;
+	}
+
+	realhandler(ev);
+}
+
+/* XXX - Ought to allow capture events as well, for completeness. But
+ * for this, need to store the capture flag in this.bindings, to keep
+ * track of it.
+ * Currently, there's a 'capture' placeholder argument, but it's
+ * ignored.
+ */
+PatEvent.bind_event = function(root, evtype, selector, handler, capture)
+{
+	/* Convenience wrappers (see comments at _enter_handler). */
+	if (evtype == "_enter")
+	{
+		var realhandler = handler;
+		evtype = "mouseover";
+		handler = function(ev) {
+			PatEvent._enter_handler(ev, realhandler)
+		};
+	} else if (evtype == "_exit")
+	{
+		var realhandler = handler;
+		evtype = "mouseout";
+		handler = function(ev) {
+			PatEvent._exit_handler(ev, realhandler)
+		};
+	}
+
+	// Add an entry to this.bindings (creating any missing bits
+	// along the way)
+	// XXX - Perhaps add another level for 'capture'?
+	if (this.bindings[selector] == null)
+		this.bindings[selector] = {};
+	if (this.bindings[selector][evtype] == null)
+		this.bindings[selector][evtype] = new Array();
+
+	this.bindings[selector][evtype].push(handler);
+
+	// Add a listener to this element and its children.
+	this._add_binding(root, evtype, selector, handler, false);
+}
+
+PatEvent._add_binding = function(node, evtype, selector, handler, capture)
+{
+	// Attach the event to the node, if it matches
+	try {
+		if (node.matchesSelector(selector))
+			node.addEventListener(evtype, handler, capture);
+	} catch (e) {
+	}
+
+	// Attach the event to any matching child nodes
+	try {
+		var allnodes = node.getElementsByTagName("*");
+	} catch (e) {
+		// Something failed. Screw it, I'm giving up.
+		return;
+	}
+	for (var i = 0, len = allnodes.length; i < len; i++)
+	{
+		var n = allnodes[i];
+		if (!n.matchesSelector(selector))
+			continue;
+		n.addEventListener(evtype, handler, capture);
+	}
+
+	// Add a DOMNodeInserted listener to this node, so we can find
+	// out if it gets any new children.
+	node.addEventListener("DOMNodeInserted",
+			      PatEvent._event_node_added,
+			      false);
+
+	// XXX - Listen for DOMAttrModified events
+}
+
+/* _event_node_added
+ * Handler for DOMNodeInserted event.
+ * See what new nodes have been inserted, and attach the events they
+ * need.
+ */
+PatEvent._event_node_added = function(ev)
+{
+	var newnode = ev.target;
+
+	// XXX - This is probably slow as molasses
+	for (var sel in PatEvent.bindings)
+	{
+		for (var evtype in PatEvent.bindings[sel])
+		{
+			for (var h in PatEvent.bindings[sel][evtype])
+				PatEvent._add_binding(newnode, evtype,
+					     sel,
+					     PatEvent.bindings[sel][evtype][h],
+					     // XXX - Need to use the correct "capture" argument.
+					     false);
 		}
 	}
 }
 
-/* bind_event
- * Bind an event of type 'evtype' (e.g., "click", "keydown",
- * "mouseover", etc.), to elements that match the given 'selector'
- * (e.g., "table.mystuff div *" or ".clickable") to the event handler
- * 'handler'.
- * 'handler' will be called with one argument: the event.
- */
-function bind_event(evtype, selector, handler)
-{
-	if (event_bindings[evtype] == null)
-	{
-		// We don't have a handler for event 'evtype'. Create
-		// one.
-		event_bindings[evtype] = {
-		};
-		event_bindings[evtype][selector] = handler;
-		document.addEventListener(evtype,
-			  function(ev) {
-				  return _event_handler(ev, event_bindings[evtype])
-			  },
-			  false);
-	}
-}
-
-/* _event_handler
- * 'ev' is the event that occurred. 'table' is the subtable of
- * 'event_bindings' for that particular event.
- */
-/* XXX - Should it be possible to bind multiple handlers to an
- * element? that is, if we have
- *	bind_event("click", ".foo", do_foo);
- *	bind_event("click", ".bar", do_bar);
- * and
- *	<button class="foo bar">
- * should both do_foo and do_bar be triggered?
- *
- * Or, perhaps more plausibly, if the selectors are ".container *" and
- * ".element" (where .element is inside .container). Should both
- * handlers be triggered?
- * Ideally, ought to call the more specific one, and allow it to
- * either preventDefault() or bubble up to the less-specific one, but
- * that seems a huge wheel to reinvent.
- */
-function _event_handler(ev, table)
-{
-	var node = ev.target;	// Node that the event occurred on
-
-	/* Find the first selector that matches the node */
-	for (var sel in table)
-	{
-//msg_add("checking selector "+sel);
-		if (node.matchesSelector(sel))
-{//msg_add("match: "+sel);
-			// Found a handler. Call it.
-			return table[sel](ev);
-}
-	}
-	return null;
-}
-#endif	// _Events_js_
+// Initialize stuff when the DOM is ready.
+// XXX - Is there a better way to do this initialization?
+document.addEventListener("DOMContentLoaded", PatEvent._init, false);
+#endif	// _PatEvent_js_
