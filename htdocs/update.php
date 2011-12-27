d7e95f7ccf89eaa5e9bc4c47676cdf9c9108be76
Andrew Arensburger <arensb@ooblick.com>
2011-12-27 13:35:00 +0000

    Get rid of unused code, in particular old 'count_display' output field.

--- htdocs/update.php
+++ htdocs/update.php
@@ -66,15 +66,9 @@ class json_output_handler extends feed_update_handler
 
 	function end_feed(&$feed)
 	{
-#		$skin = new Skin();
-#		$skin->assign('feed', $feed);
-#		$skin->assign('feed_id', $feed['id']);
-#		$skin->assign('counts', $feed['counts']);
-#		$count_display = $skin->fetch("feed-title");
 		echo jsonify('state',	"end",
 			     'feed_id',	$feed['id'],
 			     'title', $feed['title'],
-#			     'count_display',	$count_display
 			     'counts',	$feed['counts']
 			),
 			"\n";
@@ -211,11 +205,9 @@ if (is_numeric($feed_id) && is_int($feed_id+0))
 		$skin->assign('feed', $feed);
 		$skin->assign('feed_id', $feed_id);
 		$skin->assign('counts', $feed['counts']);
-		$count_display = $skin->fetch("feed-title");
 		echo jsonify('state',	"end",
 			     'feed_id',	$feed_id,
-			     'counts',	$feed['counts'],
-			     'count_display',	$count_display
+			     'counts',	$feed['counts']
 			     ),
 			"\n";
 		flush();
