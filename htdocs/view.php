0c32ad6dd40bed85efaec1c23405ea71190e8653
Andrew Arensburger <arensb@ooblick.com>
2012-02-04 16:21:28 +0000

    Removed a bunch of stuff that's no longer needed.

--- htdocs/view.php
+++ htdocs/view.php
@@ -38,7 +38,6 @@ if ($feed_id == "all")
 		"title"	=> "All feeds",
 		"id"	=> "all",
 		);
-#	$feeds = db_get_feeds();
 } else {
 	$feed = db_get_feed($feed_id);
 	if ($feed === NULL)
@@ -62,8 +61,6 @@ if (is_integer($feed_id))
 // XXX - Put these selection criteria in a JS object, which we can put
 // at the top of the rendered page, so that scripts on that page can
 // tell what we're displaying.
-#$items = db_get_some_feed_items($get_feed_args);
-$items = array();
 
 // XXX - Find out how many items there are in this list, so we can put
 // up a navigation bar.
@@ -85,85 +82,8 @@ if ($start >= $num_items)
 	$next_link = $next_link_text = NULL;
 }
 
-/* If we're reading this on a mobile device, try to convert the URL to
- * a mobile-friendly version.
- */
-// XXX - This should go in a separate plugin
-// XXX - Is it worth adding a 'mobile_url' column to the feeds table,
-// and generating these URLs when the feed is fetched?
-if ($mobile &&
-    $mobile != "iPad")		// XXX - Hack!
-{
-	// Wunderground has two mobile sites: 'i' for iPhone, 'm' for
-	// generic mobile.
-	if ($mobile == "iPhone")
-		$m_wund = "i.wund.com";		# iPhone mobile site
-	else
-		$m_wund = "m.wund.com";		# Generic mobile site
-	foreach ($items as &$i)
-	{
-		// WaPo stuff is ugly.
-		// "spf=1" is to show the full article. Leave it off
-		// to show only the first page.
-		$i['url'] = preg_replace(',^http://www\.washingtonpost\.com/wp-dyn/content/article/(\d+)/(\d+)/(\d+)/(.*)\.html\??,',
-					 'http://mobile.washingtonpost.com/rss.jsp?rssid=578819&item=http%3a%2f%2fwww.washingtonpost.com%2fwp-syndication%2farticle%2f\1%2f\2%2f\3%2f\4_mobile.xml&cid=1&spf=1&',
-					 $i['url']);
-		$i['url'] = preg_replace(',^http://(www\.)?factcheck\.org/,',
-					 "http://m.factcheck.org/",
-					 $i['url']);
-		$i['url'] = preg_replace(',^http://(www\.)?thinkprogress\.org/,',
-					 "http://m.thinkprogress.org/",
-					 $i['url']);
-		$i['url'] = preg_replace(',^http://www\.wunderground\.com/,',
-					 "http://$m_wund/",
-					 $i['url']);
-		$i['url'] = preg_replace(',^http://(\w+)\.livejournal\.com/(.*),',
-					 '\0?format=light',
-					 $i['url']);
-		$i['url'] = preg_replace(',^http://(www\.)?theonion\.com/,',
-					 "http://mobile.theonion.com/",
-					 $i['url']);
-	}
-}
-
-if ($mobile)
-{
-	// Other transformations that apply to iPad as well.
-	foreach ($items as &$i)
-	{
-		$i['url'] = preg_replace(',^http://www\.lemonde\.fr/,',
-					 'http://mobile.lemonde.fr/',
-					 $i['url']);
-		# ABC News URLs:
-		# Google News link:
-	  	# http://news.google.com/news/url?sa=t&fd=R&usg=AFQjCNGhqEe8eCdraXpBBGJwMHdr9LLqdA&url=http://abcnews.go.com/US/nantucket-massachusetts-mom-accused-killing-toddler-daughter-exorcism/story?id%3D13158775
-		#
-		# Plain link:
-		# http://abcnews.go.com/US/nantucket-massachusetts-mom-accused-killing-toddler-daughter-exorcism/story?id%3D13158775
-		#
-		# iPad link:
-		# http://abcnews.go.com/US/nantucket-massachusetts-mom-accused-killing-toddler-daughter-exorcism/t/story?id=13158775
-		#
-		# iPhone link:
-		# http://abcnews.go.com/m/story?id=13158775
-		if ($mobile == "iPad")
-			$i['url'] = preg_replace(',(http://abcnews\.go\.com/.*)/story,',
-						 '\1/t/story',
-						 $i['url']);
-		else
-			$i['url'] = preg_replace(',http://abcnews\.go\.com/.*/story,',
-						 'http://abcnews.go.com/m/story',
-						 $i['url']);
-
-		$i['url'] = preg_replace(',^http://(www\.)?npr\.org/templates/story/story.php?storyId=(129780261),',
-					 'http://www.npr.org/tablet/#story?storyId=\2',
-					 $i['url']);
-	}
-}
-
 if ($out_fmt == "json")
 {
-#	$feed['items'] = $items;
 	echo jsonify($feed);
 
 	db_disconnect();
@@ -175,7 +95,6 @@ if ($out_fmt == "xml")
 	require_once("xml-output.inc");
 				// Get print_xml() only when necessary
 
-	$feed['items'] = $items;
 	print_xml($feed);
 	db_disconnect(0);
 	exit(0);
@@ -188,7 +107,6 @@ $skin->assign('start', $start);
 $skin->assign('feed', $feed);
 #if (isset($feeds))
 #	$skin->assign('feeds', $feeds);
-#$skin->assign('items', $items);
 $skin->assign('prev_link', $prev_link);
 $skin->assign('prev_link_text', $prev_link_text);
 $skin->assign('next_link', $next_link);
