70ac8828c795abe59fa7dc086b05abd44d0c75fa
Andrew Arensburger <arensb@ooblick.com>
2012-04-18 15:11:00 +0000

    Add sentinel marks. Try to debug.

--- htdocs/skins/fancy/js/defer.js
+++ htdocs/skins/fancy/js/defer.js
@@ -1,6 +1,8 @@
 /* defer.js
  * Defines the defer() function, for browsers that don't have it.
  */
+#ifndef _defer_js_
+#define _defer_js_
 if (!("defer" in Function.prototype))
 {
 	/* Function.defer
@@ -13,9 +15,12 @@ if (!("defer" in Function.prototype))
 		function(n,o) {
 			// Get arguments as array
 			var a = [];
-			for(var i=2; i<arguments.length; i++)
+			for(var i=1; i<arguments.length; i++)
 				a.push(arguments[i]);
 			var that = this;
+console.debug("defer("+n+", "+o+")");
+console.debug(a);
 			window.setTimeout(function(){return that.apply(o,a);},n);
 		};
 }
+#endif	// _defer_js_
