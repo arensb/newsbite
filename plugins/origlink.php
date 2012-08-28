6d5b8e5ab83b146fe60a2eb1eb861d2a2b040c76
Andrew Arensburger <arensb@ooblick.com>
2012-08-28 12:58:44 +0000

    (bug fix) Sometimes an article's url would be "Array" due to multiple <feedburner:origLink> elements. Fixed.

--- plugins/origlink.php
+++ plugins/origlink.php
@@ -12,7 +12,17 @@ function feedburner_origlink_hook($nodename, &$retval, &$context)
 	{
 		if (isset($retval['url']))
 			$retval['oldurl'] = $retval['url'];
-		$retval['url'] = $retval['feedburner:origLink'];
+
+		# Apparently some feeds have multiple <feedburner:origLink>
+		# elements (Pharyngula is the top offender, with 5). In the
+		# cases I've seen, the first one links to the original post,
+		# and the others are just links back to feedburner.
+		#	At any rate, we need to check for this; otherwise,
+		# the post's 'url' winds up being the string "Array".
+		if (is_array($retval['feedburner:origLink']))
+			$retval['url'] = $retval['feedburner:origLink'][0];
+		else
+			$retval['url'] = $retval['feedburner:origLink'];
 	}
 }
 
