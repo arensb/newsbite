15cd5776f401411f94c323f07aeb442e807ce04f
Andrew Arensburger <arensb@ooblick.com>
2012-09-01 17:30:58 +0000

    Remove a bunch of unneeded stuff: hooks, "start" parameter, leftover code for fetching items, non-HTML output.

--- htdocs/view.php
+++ htdocs/view.php
@@ -5,9 +5,6 @@
 require_once("common.inc");
 require_once("database.inc");
 require_once("skin.inc");
-require_once("hooks.inc");
-
-load_hooks(PLUGIN_DIR);
 
 $feed_id = $_REQUEST['id'];		// ID of feed to show
 /* Make sure $feed_id is an integer */
@@ -17,17 +14,6 @@ elseif ($feed_id == "all")
 	;
 else
 	abort("Invalid feed ID: \"$feed_id\".");
-
-if (isset($_REQUEST['s']))
-{
-	$start = $_REQUEST['s'];		// Skip first $start items
-
-	/* Make sure $feed_id is an integer */
-	if (!is_numeric($start) || !is_integer($start+0))
-		/* Ignore illegal values. */
-		$start = 0;
-} else
-	$start = 0;
 
 if ($feed_id == "all")
 {
@@ -47,38 +33,6 @@ if ($feed_id == "all")
 # Things we don't want to send to the client
 unset($feed['username']);
 unset($feed['passwd']);
-
-$num_items = 25;		// How many items to show
-		// XXX - Should probably be a parameter
-
-$get_feed_args = array(
-	"read"		=> false,
-	"max_items"	=> $num_items,
-	"start_at"	=> $start
-	);
-if (is_integer($feed_id))
-	$get_feed_args['feed_id'] = $feed_id;
-// XXX - Put these selection criteria in a JS object, which we can put
-// at the top of the rendered page, so that scripts on that page can
-// tell what we're displaying.
-
-if ($out_fmt == "json")
-{
-	echo jsonify($feed);
-
-	db_disconnect();
-	exit(0);
-}
-
-if ($out_fmt == "xml")
-{
-	require_once("xml-output.inc");
-				// Get print_xml() only when necessary
-
-	print_xml($feed);
-	db_disconnect(0);
-	exit(0);
-}
 
 /* If we get this far, user has requested HTML output */
 $skin = new Skin();
