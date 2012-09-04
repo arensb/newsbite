74987121517d75d66b9535db5a06ce9052a571d0
Andrew Arensburger <arensb@ooblick.com>
2012-09-04 14:15:30 +0000

    Add hook-related stuff, since we need it here (and it's not in htdocs/view.php anymore).

--- htdocs/skins/fancy/view.php
+++ htdocs/skins/fancy/view.php
@@ -1,4 +1,7 @@
 <?
+require_once("hooks.inc");
+load_hooks(PLUGIN_DIR);
+
 // Give some of the skin variables shorter names
 $skin_dir = $skin_vars['skin'];
 $feed = &$skin_vars['feed'];
@@ -114,6 +117,7 @@ else
 
 <?if (isset($feed['description']) && $feed['description'] != ""):
 	$description = $feed['description'];
+	# XXX - Need to move clean-html to lib/parse-feed.inc
 	run_hooks("clean-html", array(&$description))
 ?>
 <div class="feed-description"><?=$description?></div>
