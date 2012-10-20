09fedd734c23280a6306575cb91c4aa8f4a11d3b
Andrew Arensburger <arensb@ooblick.com>
2012-10-20 16:42:05 +0000

    Minor cleanup.

--- plugins/slashdot.php
+++ plugins/slashdot.php
@@ -3,10 +3,8 @@
 
 function slashdot_noise_hook($nodename, &$retval, &$context)
 {
-#echo "Inside slashdot_noise_hook<br/>\n";
 	if (!is_string($retval))
 		return;
-#echo "(slashdot_noise) before: <pre>["; print_r($retval); echo "]</pre>\n";
 
 	// Facebook and Twitter links
 	$retval = preg_replace('{<a class="slashpop".*?</a>}',
@@ -22,13 +20,10 @@ function slashdot_noise_hook($nodename, &$retval, &$context)
 			       '',
 			       $retval);
 
-#echo "(slashdot_noise) after 1: <pre>["; print_r($retval); echo "]</pre>\n";
 	# Unnecessary link to story
-	# XXX - Should this check to see whether the link is the same as in the "header"?
 	$retval = preg_replace('{\s*<p><a href="http://(\w+\.)?slashdot.org/story/[^\"]*">Read more of this story</a> at Slashdot.</p>}',
 			       '',
 			       $retval);
-#echo "(slashdot_noise) after 2: <pre>["; print_r($retval); echo "]</pre>\n";
 }
 
 add_hook("summary", "slashdot_noise_hook");
