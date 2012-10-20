961c3514b2efbd45c1606e60b1c39a4e26fa960d
Andrew Arensburger <arensb@ooblick.com>
2012-10-20 16:33:57 +0000

    Minor cleanup.

--- plugins/bugs.php
+++ plugins/bugs.php
@@ -1,16 +1,11 @@
 <?php
-/* Remove web bugs (images of size 0x0 or 1x1) from feeds */
-
 /* webbug_hook
- * Remove web bugs: images with width=1 and height=1
+ * Remove web bugs: images with width=1 and height=1 or less.
  */
-/* XXX - This should go in a separate file, as a plug-in */
 function webbug_hook($nodename, &$retval, &$context)
 {
 	if (!is_string($retval))
 		return;
-#echo "Inside webbug_hook($nodename)<br/>\n";
-#echo "(webbug) before: <pre>[", htmlentities($retval), "]</pre>\n";
 	$retval = preg_replace('{<img(\s+\w+=\"[^\"]*\")* (height="[01]" width="[01]"|width="[01]" height="[01]")(\s+\w+=\"[^\"]*\")*\s*/>\r?\n?}',
 			       '',
 			       $retval);
@@ -19,7 +14,6 @@ function webbug_hook($nodename, &$retval, &$context)
 	$retval = preg_replace('{<img(\s+\w+=\'[^\']*\')* (height=\'[01]\' width=\'[01]\'|width=\'[01]\' height=\'[01]\').*?/>\r?\n?}',
 			       '',
 			       $retval);
-#echo "(webbug) after 2: <pre>[", htmlentities($retval), "]</pre>\n";
 }
 
 add_hook("summary", "webbug_hook");
