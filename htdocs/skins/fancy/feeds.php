6ef1c53324036cbcf21208ff5f4ec7c72011e58d
Andrew Arensburger <arensb@ooblick.com>
2013-03-08 14:39:16 +0000

    Use manifest, for offline reading.

--- htdocs/skins/fancy/feeds.php
+++ htdocs/skins/fancy/feeds.php
@@ -6,7 +6,7 @@ $skin_dir = $skin_vars['skin'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html manifest="newsbite.manifest">
 <head>
 <title>NewsBite: Feeds</title>
 <link rel="stylesheet" type="text/css" href="skins/<?=$skin_dir?>/style.css" media="all" />
--- htdocs/skins/fancy/view.php
+++ htdocs/skins/fancy/view.php
@@ -10,7 +10,7 @@ echo '<', '?xml version="1.0" encoding="UTF-8"?', ">\n";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html manifest="newsbite.manifest">
 <head>
 <script type="text/javascript">
 // Set various useful variables to pass on to scripts
