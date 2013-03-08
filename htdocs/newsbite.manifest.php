8d083bbba7e451cb13c8b79326446522bb0bf83b
Andrew Arensburger <arensb@ooblick.com>
2013-03-08 14:38:25 +0000

    Generate manifest, for web applications.

---
+++ htdocs/newsbite.manifest.php
@@ -0,0 +1,18 @@
+CACHE MANIFEST
+# Nonce: <?php echo strftime("%Y-%m-%dT%H:%M:%S"),"\n" ?>
+index.php
+view.php
+<?# The files after the "NETWORK:" line will be fetched each time.?>
+#NETWORK:
+<?#
+# Skin files:
+?>
+#CACHE:
+skins/fancy/style.css
+skins/fancy/index.css
+skins/fancy/feeds.js
+skins/fancy/gray-bg.jpg
+skins/fancy/Ajax-loader.gif
+skins/fancy/Attraction_transfer_icon.gif
+skins/fancy/view.css
+skins/fancy/view.js
