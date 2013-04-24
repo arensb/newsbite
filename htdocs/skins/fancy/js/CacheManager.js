1b0ce842614de2cfebb8a875738e500c3e8b99ac
Andrew Arensburger <arensb@ooblick.com>
2013-04-24 13:32:16 +0000

    (minor) Removed some unwanted trace statements.

--- htdocs/skins/fancy/js/CacheManager.js
+++ htdocs/skins/fancy/js/CacheManager.js
@@ -154,8 +154,6 @@ function _purge_stuff(todelete)
 
 	var i = 0;	// Number of items deleted in this run
 	var key;
-console.log("purging "+todelete.length+" items");
-console.trace();
 	while ((key = todelete.shift()) != null)
 	{
 		localStorage.removeItem(key);
