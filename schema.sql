94b7e85b9bbaf2b69505217b8fa500fdf4559d3e
Andrew Arensburger <arensb@ooblick.com>
2014-11-18 12:18:38 -0500

    Fix schema to tighten group_members a bit.

--- schema.sql
+++ schema.sql
@@ -48,7 +48,8 @@ DELETE FROM groups WHERE name="dummy";
 '*/
 CREATE TABLE group_members (
 	member		INT		NOT NULL,
-	parent		INT		NOT NULL DEFAULT -1
+	parent		INT		NOT NULL DEFAULT -1,
+	UNIQUE KEY (member, parent)
 )
 
 CREATE TABLE feeds (
