8f3761225d2d420e7686cf375fbb629a6ba87d96
Andrew Arensburger <arensb@ooblick.com>
2014-11-20 15:07:52 -0500

    A somewhat less stupid way of creating group -1.

--- fix-schema-1.0.0-87-g6d3303b.sql
+++ fix-schema-1.0.0-87-g6d3303b.sql
@@ -1,12 +1,9 @@
 /* Create the mandatory "All" group, with ID -1 */
-INSERT INTO groups VALUES (-1, -1, "All");
-
-/* Hack to make auto-increment work. See schema.sql. */
-INSERT INTO groups (name) VALUES ("dummy");
-DELETE FROM groups WHERE name="dummy";
+INSERT INTO groups (name, parent) VALUES ("All", -1);
+UPDATE groups SET id=-1 WHERE id=last_insert_id();
 
 CREATE TABLE group_members (
 	member		INT		NOT NULL,
 	parent		INT		NOT NULL DEFAULT -1,
 	UNIQUE KEY (member, parent)
-)
+);
