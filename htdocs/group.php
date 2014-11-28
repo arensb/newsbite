e204bcb0835fd8bdd803cab5894a78d1db213224
Andrew Arensburger <arensb@ooblick.com>
2014-11-28 01:05:36 -0500

    Moved creating tree of groups to its own module.

--- htdocs/group.php
+++ htdocs/group.php
@@ -3,7 +3,8 @@
  * Edit groups and whatnot.
  */
 require_once("common.inc");
-require_once("database.inc");
+#require_once("database.inc");
+require_once("group.inc");
 require_once("skin.inc");
 
 /* XXX - Should take a command and do something intelligent.
@@ -13,38 +14,6 @@ require_once("skin.inc");
  * - move a group to a different parent
  */
 
-$groups = db_get_groups();
-
-echo "groups: <pre>", htmlspecialchars(print_r($groups, TRUE)), "</pre>";
-
-listgroup(-1);
-
-function listgroup($gid)
-{
-	global $groups;
-
-	$group = $groups[$gid];
-	$children = array();
-	foreach ($groups as $child)
-	{
-		if ($child['parent'] == $gid && $child['id'] != -1)
-			$children[] = $child['id'];
-	}
-echo "children: ", print_r($children, TRUE), "<br/>\n";
-	echo "<span class=\"groupname\">",
-		htmlspecialchars($group['name']),
-		"</span>";
-	if (count($children) > 0)
-	{
-		echo "<ul>";
-		foreach ($children as $c)
-		{
-			$child = $groups[$c];
-			echo "<li>";
-			listgroup($c);
-			echo "</li>";
-		}
-		echo "</ul>";
-	}
-}
+$groups = group_tree(TRUE);
+print_r($groups);
 ?>
