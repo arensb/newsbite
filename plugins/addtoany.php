a29d0037d6bbea3c0b27393b6bf5123c450320bb
Andrew Arensburger <arensb@ooblick.com>
2014-12-02 17:20:21 -0500

    Bug fix: wasn't catching all of the addtoany classes.

--- plugins/addtoany.php
+++ plugins/addtoany.php
@@ -8,7 +8,7 @@ function strip_addtoany($nodename, &$retval, &$context)
 	if (!is_string($retval))
 		return;
 #echo "a2a before: <tt>"; print_r($retval);
-	$retval = preg_replace('{^<p><a class="a2a_dd.*}m',
+	$retval = preg_replace('{^<p><a class="a2a_(dd|button|target).*}m',
 			       '',
 			       $retval);
 #echo "a2a after: <tt>"; print_r($retval);
