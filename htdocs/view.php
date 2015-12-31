e85dae71b5b6865749f6f279752c86a0a7ec1dea
Andrew Arensburger <arensb@ooblick.com>
2015-12-31 01:28:36 -0500

    Minor cleanup.

--- htdocs/view.php
+++ htdocs/view.php
@@ -3,16 +3,6 @@
  * Display a feed.
  */
 require_once("common.inc");
-#require_once("skin.inc");
-#
-#/* If we get this far, user has requested HTML output */
-#$skin = new Skin();
-#
-### XXX - Debugging
-#$skin->assign('auth_user', $auth_user);
-#$skin->assign('auth_expiration', strftime("%c", $auth_expiration));
-### XXX - end debugging
-#$skin->display("view");
 
 # Tell the client to cache this for a week
 $tomorrow = new DateTime("now + 1 week");
