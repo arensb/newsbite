516894cefc12b465afc41e269dc8380795d5f1d4
Andrew Arensburger <arensb@ooblick.com>
2016-01-22 21:46:05 -0500

    Add more things to the manifest.

--- htdocs/newsbite.manifest.php
+++ htdocs/newsbite.manifest.php
@@ -1,10 +1,48 @@
 CACHE MANIFEST
 # Nonce: <?php echo strftime("%Y-%m-%dT%H:%M:%S"),"\n" ?>
+addfeed.php
+editfeed.php
+feeds.php
+group.php
 index.php
+unsubscribe.php
 view.php
+js/feeds.js
+js/group.js
 js/jquery.js
+js/view.js
+css/abort.css
+css/addfeed.css
+css/android.css
+css/desktop.css
+css/editfeed.css
+css/editgroups.css
+css/index.css
+css/ipad.css
+css/iphone.css
+css/opml.css
+css/style.css
+css/unsubscribe.css
+css/view.css
+fonts/Roboto-Regular-webfont.woff
+fonts/Roboto-Thin-webfont.woff
+images/Ajax-loader.gif
+images/Attention_niels_epting.png
+images/Attraction_transfer_icon.gif
+images/Loading.gif
+images/gray-bg.jpg
+images/newsbite-icon.png
+images/newsbite-splash-ipad.png
 <?php // The files after the "NETWORK:" line will be fetched each time. ?>
 #NETWORK:
+loadopml.php
+login.php
+markitems.php
+opml.php
+sync.php
+update.php
+<?php // The files after "CACHE:" will be cached ?>
+#CACHE:
 # Fetch anything else from the network, always.
 NETWORK:
 *
