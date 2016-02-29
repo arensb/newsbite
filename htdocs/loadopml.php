ce60c6a5c8984ade45e6f9a3bf8398690337ae76
Andrew Arensburger <arensb@ooblick.com>
2016-02-29 17:43:57 -0500

    Show the user a message saying whether the OPML file was uploaded successfully.

--- htdocs/loadopml.php
+++ htdocs/loadopml.php
@@ -27,6 +27,7 @@ if (!isset($_FILES['opml']) || $_FILES['opml'] == ""):
 <p>Select an OPML file to upload.</p>
 
 <input type="file" name="opml-file" id="opml-file"/>
+<div id="opml-status"></div>
 
 <script type="text/javascript">
 // XXX - Move this code to a .js file.
@@ -57,6 +58,37 @@ function upload_opml_file(f)
 		// XXX - Why is "PUT" not allowed?
 		req.setRequestHeader("Content-Type",
 				     "text/xml; charset=UTF-8");
+		req.onreadystatechange = function() {
+			// XXX - This should tell the user whether the
+			// upload was successful. Ideally, go through
+			// the list of feeds, and say which ones had
+			// errors.
+			if (req.readyState !== XMLHttpRequest.DONE)
+				return;
+			console.debug("OPML XMLHttpRequest request returned status "+
+				    req.status);
+			if (req.status !== 200)
+			{
+				// Something failed
+				console.debug(req.responseText);
+				$("#opml-status").html("<b>Status:</b> " +
+						       req.status);
+				return;
+			}
+			var response;
+			try {
+				response = JSON.parse(req.responseText);
+			} catch (e) {
+				console.debug("Can't parse JSON response: ", e);
+				$("#opml-status").html("Can't parse REST response JSON");
+				return;
+			}
+
+			$("#opml-status").html("<b>Status:</b> " +
+					       response.status +
+					       ": " +
+					       response.message);
+		};
 		req.send(contents);
 			// XXX - Error-checking.
 	}
