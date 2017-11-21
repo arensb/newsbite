6147b5ebc23635e3bf89d896cbd066942c0db4b2
Andrew Arensburger <arensb>
2017-11-21 09:37:04 -0500

    Add id-from option to schema, with a default.

---
+++ fix-schema-1.1.0-294-g41fd524.sql
@@ -0,0 +1,3 @@
+# Adding a new option
+INSERT INTO feed_options (feed_id, name, value) VALUES
+	(-1, "id-from", "guid");	# Where to get unique post ID.
--- schema.sql
+++ schema.sql
@@ -21,7 +21,8 @@ DEFAULT CHARSET=utf8mb4;
 
 /* Set defaults for options. */
 INSERT INTO feed_options (feed_id, name, value) VALUES
-	(-1, "autodelete", 90);		# When to expunge old posts.
+	(-1, "autodelete", 90),		# When to expunge old posts.
+	(-1, "id-from", "guid");	# Where to get unique post ID.
 
 /* groups
  * For grouping feeds into nested groups.
